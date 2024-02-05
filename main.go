d5e3a3781a5507c8a5c172466a0bd13aeaddd714
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-02-05 10:50:08 -0800

    fixes lint issue

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ var (
 	// to appropriate values for the compiled binary.
 	version string = "dev"
 	// this is replaced by goreleaser during build time and is only used
-	// if no value is provided to the provider in terraform or via the environment
+	// if no value is provided to the provider in terraform or via the environment.
 	defaultEndpoint string = "localhost:8080"
 
 	// goreleaser can pass other information to the main package, such as the specific commit
