da9af0d17fde55a7fd060b5d076b4b608aef8123
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-02-06 14:22:55 -0800

    mysql mysql test

--- internal/provider/job_resource_test.go
+++ internal/provider/job_resource_test.go
@@ -491,3 +491,71 @@ resource "neosync_job" "job1" {
 		},
 	})
 }
+
+func TestAcc_Job_Mysql_Mysql(t *testing.T) {
+	name := acctest.RandString(10)
+
+	config := fmt.Sprintf(`
+resource "neosync_connection" "source" {
+	name = "%s-src"
+
+	mysql = {
+		url = "test-url"
+	}
+}
+resource "neosync_connection" "destination" {
+	name = "%s-dest"
+
+	mysql = {
+		url = "test-url2"
+	}
+}
+
+resource "neosync_job" "job1" {
+	name = "%s"
+	source = {
+		mysql = {
+			halt_on_new_column_addition = false
+			connection_id = neosync_connection.source.id
+		}
+	}
+	destinations = [
+		{
+			connection_id = neosync_connection.destination.id
+			mysql = {
+				init_table_schema = false
+				truncate_table = {
+					truncate_before_insert = true
+				}
+			}
+		}
+	]
+	mappings = [
+		{
+			schema = "public"
+			table = "users"
+			column = "id"
+			transformer = {
+				source = "passthrough"
+				config = {
+					passthrough = {}
+				}
+			}
+		}
+	]
+}
+	`, name, name, name)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("neosync_job.job1", "id"),
+				),
+			},
+		},
+	})
+}
