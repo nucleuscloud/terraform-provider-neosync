6608a5f88c200fc2331d9af60c9117a15f894c37
Nick Z <2420177+nickzelei@users.noreply.github.com>
2024-02-06 13:47:58 -0800

    adds some tests for job

--- internal/provider/job_resource_test.go
+++ internal/provider/job_resource_test.go
@@ -76,3 +76,418 @@ resource "neosync_job" "job1" {
 		},
 	})
 }
+
+func TestAcc_Job_Pg_Pg_Mappings(t *testing.T) {
+	name := acctest.RandString(10)
+
+	config := fmt.Sprintf(`
+resource "neosync_connection" "source" {
+	name = "%s-src"
+
+	postgres = {
+		url = "test-url"
+	}
+}
+resource "neosync_connection" "destination" {
+	name = "%s-dest"
+
+	postgres = {
+		url = "test-url2"
+	}
+}
+
+resource "neosync_job" "job1" {
+	name = "%s"
+	source = {
+		postgres = {
+			halt_on_new_column_addition = false
+			connection_id = neosync_connection.source.id
+		}
+	}
+	destinations = [
+		{
+			connection_id = neosync_connection.destination.id
+			postgres = {
+				init_table_schema = false
+				truncate_table = {
+					truncate_before_insert = true
+					cascade = true
+				}
+			}
+		}
+	]
+	mappings = [
+		{
+			schema = "public"
+			table = "users"
+			column = "id"
+			transformer = {
+				source = "passthrough"
+				config = {
+					passthrough = {}
+				}
+			}
+		}
+	]
+}
+	`, name, name, name)
+
+	config1 := fmt.Sprintf(`
+resource "neosync_connection" "source" {
+	name = "%s-src"
+
+	postgres = {
+		url = "test-url"
+	}
+}
+resource "neosync_connection" "destination" {
+	name = "%s-dest"
+
+	postgres = {
+		url = "test-url2"
+	}
+}
+
+resource "neosync_job" "job1" {
+	name = "%s"
+	source = {
+		postgres = {
+			halt_on_new_column_addition = false
+			connection_id = neosync_connection.source.id
+		}
+	}
+	destinations = [
+		{
+			connection_id = neosync_connection.destination.id
+			postgres = {
+				init_table_schema = false
+				truncate_table = {
+					truncate_before_insert = true
+					cascade = true
+				}
+			}
+		}
+	]
+	mappings = [
+		{
+			schema = "public"
+			table = "users"
+			column = "id"
+			transformer = {
+				source = "passthrough"
+				config = {
+					passthrough = {}
+				}
+			}
+		},
+		{
+			schema = "public"
+			table = "users"
+			column = "name"
+			transformer = {
+				source = "passthrough"
+				config = {
+					passthrough = {}
+				}
+			}
+		}
+	]
+}
+	`, name, name, name)
+
+	config2 := fmt.Sprintf(`
+resource "neosync_connection" "source" {
+	name = "%s-src"
+
+	postgres = {
+		url = "test-url"
+	}
+}
+resource "neosync_connection" "destination" {
+	name = "%s-dest"
+
+	postgres = {
+		url = "test-url2"
+	}
+}
+
+resource "neosync_job" "job1" {
+	name = "%s"
+	source = {
+		postgres = {
+			halt_on_new_column_addition = false
+			connection_id = neosync_connection.source.id
+		}
+	}
+	destinations = [
+		{
+			connection_id = neosync_connection.destination.id
+			postgres = {
+				init_table_schema = false
+				truncate_table = {
+					truncate_before_insert = true
+					cascade = true
+				}
+			}
+		}
+	]
+	mappings = [
+		{
+			schema = "public"
+			table = "users"
+			column = "name"
+			transformer = {
+				source = "passthrough"
+				config = {
+					passthrough = {}
+				}
+			}
+		}
+	]
+}
+	`, name, name, name)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("neosync_job.job1", "id"),
+				),
+			},
+			{
+				Config: config1,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("neosync_job.job1", "id"),
+				),
+			},
+			{
+				Config: config2,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("neosync_job.job1", "id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAcc_Job_Pg_Pg_Destinations(t *testing.T) {
+	name := acctest.RandString(10)
+
+	config := fmt.Sprintf(`
+resource "neosync_connection" "source" {
+	name = "%s-src"
+
+	postgres = {
+		url = "test-url"
+	}
+}
+resource "neosync_connection" "destination" {
+	name = "%s-dest"
+
+	postgres = {
+		url = "test-url2"
+	}
+}
+
+resource "neosync_job" "job1" {
+	name = "%s"
+	source = {
+		postgres = {
+			halt_on_new_column_addition = false
+			connection_id = neosync_connection.source.id
+		}
+	}
+	destinations = [
+		{
+			connection_id = neosync_connection.destination.id
+			postgres = {
+				init_table_schema = false
+				truncate_table = {
+					truncate_before_insert = true
+					cascade = true
+				}
+			}
+		}
+	]
+	mappings = [
+		{
+			schema = "public"
+			table = "users"
+			column = "id"
+			transformer = {
+				source = "passthrough"
+				config = {
+					passthrough = {}
+				}
+			}
+		}
+	]
+}
+	`, name, name, name)
+
+	config1 := fmt.Sprintf(`
+resource "neosync_connection" "source" {
+	name = "%s-src"
+
+	postgres = {
+		url = "test-url"
+	}
+}
+resource "neosync_connection" "destination" {
+	name = "%s-dest"
+
+	postgres = {
+		url = "test-url2"
+	}
+}
+resource "neosync_connection" "destination2" {
+	name = "%s-dest2"
+
+	postgres = {
+		url = "test-url2"
+	}
+}
+
+resource "neosync_job" "job1" {
+	name = "%s"
+	source = {
+		postgres = {
+			halt_on_new_column_addition = false
+			connection_id = neosync_connection.source.id
+		}
+	}
+	destinations = [
+		{
+			connection_id = neosync_connection.destination.id
+			postgres = {
+				init_table_schema = false
+				truncate_table = {
+					truncate_before_insert = true
+					cascade = true
+				}
+			}
+		},
+		{
+			connection_id = neosync_connection.destination2.id
+			postgres = {
+				init_table_schema = false
+				truncate_table = {
+					truncate_before_insert = true
+					cascade = true
+				}
+			}
+		},
+	]
+	mappings = [
+		{
+			schema = "public"
+			table = "users"
+			column = "id"
+			transformer = {
+				source = "passthrough"
+				config = {
+					passthrough = {}
+				}
+			}
+		}
+	]
+}
+	`, name, name, name, name)
+
+	config2 := fmt.Sprintf(`
+resource "neosync_connection" "source" {
+	name = "%s-src"
+
+	postgres = {
+		url = "test-url"
+	}
+}
+resource "neosync_connection" "destination2" {
+	name = "%s-dest2"
+
+	postgres = {
+		url = "test-url2"
+	}
+}
+resource "neosync_connection" "destination3" {
+	name = "%s-dest3"
+
+	postgres = {
+		url = "test-url3"
+	}
+}
+
+resource "neosync_job" "job1" {
+	name = "%s"
+	source = {
+		postgres = {
+			halt_on_new_column_addition = false
+			connection_id = neosync_connection.source.id
+		}
+	}
+	destinations = [
+		{
+			connection_id = neosync_connection.destination2.id
+			postgres = {
+				init_table_schema = true
+				truncate_table = {
+					truncate_before_insert = true
+					cascade = true
+				}
+			}
+		},
+		{
+			connection_id = neosync_connection.destination3.id
+			postgres = {
+				init_table_schema = false
+				truncate_table = {
+					truncate_before_insert = true
+					cascade = true
+				}
+			}
+		},
+	]
+	mappings = [
+		{
+			schema = "public"
+			table = "users"
+			column = "id"
+			transformer = {
+				source = "passthrough"
+				config = {
+					passthrough = {}
+				}
+			}
+		}
+	]
+}
+	`, name, name, name, name)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("neosync_job.job1", "id"),
+				),
+			},
+			{
+				Config: config1,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("neosync_job.job1", "id"),
+				),
+			},
+			{
+				Config: config2,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("neosync_job.job1", "id"),
+				),
+			},
+		},
+	})
+}
